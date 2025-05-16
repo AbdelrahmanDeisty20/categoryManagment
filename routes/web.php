f00725d2d44f95fe5f1ab36062925beaaf161a0a
AbdelrahmanDeisty20 <abooda601@gmail.com>
2025-05-16 10:05:03 +0300

    task

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,15 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('index');
+
 Route::get('/categories/paginate', [CategoryController::class, 'ajaxPaginate'])->name('categories.paginate');
+
 Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
+
 Route::post('/categories/update', [CategoryController::class, 'update'])->name('categories.update');
+
 Route::delete('/categories/delete', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
 Route::get('/categories/search', [CategoryController::class, 'search'])->name('categories.search');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
